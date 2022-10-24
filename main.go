378f71f9e41c233a15815088dd086d86c4ac8419
interads <syarifuddin@interads.co.id>
2022-10-24 17:34:25 +0700

    store product

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -17,6 +18,8 @@ type Product struct {
 	Name        string
 	Description string
 	Price       float64
+	Image       string
+	Status      int
 	Created_At  time.Time
 	Updated_At  time.Time
 }
@@ -43,7 +46,7 @@ func connectDB() *sql.DB {
 
 func index(w http.ResponseWriter, r *http.Request) {
 
-	rows, err := connectDB().Query(`SELECT id, name, description,price FROM products`)
+	rows, err := connectDB().Query(`SELECT id, name, description,image,price,status FROM products`)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -57,7 +60,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 	for rows.Next() {
 		var p Product
 
-		err := rows.Scan(&p.Id, &p.Name, &p.Description, &p.Price)
+		err := rows.Scan(&p.Id, &p.Name, &p.Description, &p.Image, &p.Price, &p.Status)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -73,8 +76,13 @@ func index(w http.ResponseWriter, r *http.Request) {
 }
 
 func create(w http.ResponseWriter, r *http.Request) {
+
+	data := PageData{
+		PageTitle: "Create Product",
+	}
+
 	var view = template.Must(template.ParseFiles("views/create.html"))
-	view.Execute(w, nil)
+	view.Execute(w, data)
 }
 
 func edit(w http.ResponseWriter, r *http.Request) {
@@ -83,8 +91,38 @@ func edit(w http.ResponseWriter, r *http.Request) {
 }
 
 func store(w http.ResponseWriter, r *http.Request) {
-	var view = template.Must(template.ParseFiles("views/edit.html"))
-	view.Execute(w, nil)
+
+	if r.Method != http.MethodPost {
+		http.Redirect(w, r, "/create", r.Response.StatusCode)
+	}
+
+	product := Product{
+		Id:          0,
+		Name:        r.FormValue("name"),
+		Description: r.FormValue("description"),
+		Price:       0,
+		Image:       "-",
+		Status:      1,
+		Created_At:  time.Now(),
+		Updated_At:  time.Now(),
+	}
+
+	result, err := connectDB().Exec(`INSERT INTO products (id,name, description,price,image,status, created_at,updated_at) VALUES (?,?, ?, ?,?, ?, ?,?)`, nil, product.Name, product.Description, product.Price, product.Image, product.Status, product.Created_At, product.Updated_At)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	id, err := result.LastInsertId()
+
+	if id != 0 {
+		http.Redirect(w, r, "/", 200)
+		return
+	} else {
+		fmt.Println(err)
+		http.Redirect(w, r, "/create", r.Response.StatusCode)
+		return
+	}
+
 }
 
 func update(w http.ResponseWriter, r *http.Request) {
